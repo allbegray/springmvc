48d9e8932fea98b16b1de1781d3510b61e5ac87a
이용홍 <allbegray@gmail.com>
2017-08-02 16:21:36 +0900

    json 반환 가능 하도록 수정

--- src/main/java/springmvc/controller/HelloController.java
+++ src/main/java/springmvc/controller/HelloController.java
@@ -3,6 +3,10 @@ package springmvc.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 public class HelloController {
@@ -10,6 +14,15 @@ public class HelloController {
     @RequestMapping("/hello")
     public void hello(Model model) {
         model.addAttribute("name", "한글되냐?");
+    }
+
+    @RequestMapping("/json")
+    @ResponseBody
+    public Map<String, String> json() {
+        Map<String, String> data = new HashMap<>();
+        data.put("1", "1");
+        data.put("2", "2");
+        return data;
     }
 
 }
